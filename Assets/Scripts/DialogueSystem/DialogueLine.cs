07b515e894436943ad9cf10e2738e1faf33f68f9
Seretaneva <seretan30@gmail.com>
2025-02-28 20:30:52 +0200

    Init Unity Project

---
+++ Assets/Scripts/DialogueSystem/DialogueLine.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDialogueLine", menuName ="Dialogue/Line")]
+public class DialogueLine : ScriptableObject
+{
+public string speakerName;
+[TextArea(3,5)]public string dialogText;
+public Sprite characterSprite;
+
+public DialogueTarget dialogueTarget;
+
+[Header("Audio")]
+public AudioClip spokenText;
+public AudioClip backgroundMusic;
+public AudioClip soundEffect;
+
+[Header("Animation")]
+public float animationDuration = 1;
+
+public DialogueAnimation animationType;
+
+}
+
+public enum DialogueTarget
+{
+LeftImage,
+RightImage,
+CenterImage
+}
+
+public enum DialogueAnimation{
+    
+    None,
+    EnteringScene,
+    ExitingScene,
+    Jumping,
+    Rotating,
+    Scaling,
+    Shaking
+
+
+}
